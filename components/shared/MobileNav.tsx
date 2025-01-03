8fa0a9575fb562de8dd2aaa0ac72713705f44ce4
msinha569 <msinha569@gmail.com>
2025-01-03 13:47:45 +0530

    eslint errors while deploying

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 import {
     Sheet,
     SheetContent,
-    SheetDescription,
-    SheetHeader,
-    SheetTitle,
     SheetTrigger,
   } from "@/components/ui/sheet"
 import Link from 'next/link'
